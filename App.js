c0677fe0fb94608e362369aee9eb1d1181173688
suzen01 <32843763+suzen01@users.noreply.github.com>
2022-01-09 23:47:28 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './component/Component'
+import {useEffect} from "react";
+
+
+function App() {
+  return (
+    <div>
+      <h1> hello </h1>
+     useEffect(() => () ;
+     fetchData();)
+   </div>
+
+
+  );
+    
+}
+
+export default App;
